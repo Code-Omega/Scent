4891a0e857798670ddb1075b46ddc411553d78c8
Code-Omega <ltomzh@gmail.com>
2019-03-21 23:32:35 -0400

    added path

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const express = require("express");
+const path = require('path');
 const bodyParser = require("body-parser");
 const Noun = require("./schemas/noun");
 const Review = require("./schemas/review");
